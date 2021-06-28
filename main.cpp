225fa956e79b051841ea6108699e58a704e6a19f
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-06-27 20:10:44 -0600

    8 - Getters And Setters

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 class Character
 {
 public:
-
+    Vector2 getWorldPos() { return worldPos; }
 private:
-
+    Texture2D texture;
+    Texture2D idle;
+    Texture2D run;
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f};
 };
 
 int main()
