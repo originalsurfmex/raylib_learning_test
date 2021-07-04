4de838c3c681aa6518a54906b4449dcf5ba2042b
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-07-04 12:27:29 -0600

    17 - Prop Collision

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
-    Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}
+    };
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -28,7 +31,11 @@ int main()
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        rock.Render(knight.getWorldPos());
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
 
         knight.tick(GetFrameTime());
         // check map bounds
@@ -38,6 +45,14 @@ int main()
             knight.getWorldPos().y + windowHeight > map.height * mapScale)
         {
             knight.undoMovement();
+        }
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
         }
 
         EndDrawing();
